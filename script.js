d5fba67fd97e45b0db7da6ab52486234b90ff675
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-20 10:59:02 -0400

    mvp commot for prompts

---
+++ script.js
@@ -0,0 +1,75 @@
+// What is the keyword that displays a box requesting user input?
+prompt
+
+// What is the keyword in a statement that shares most of the syntax of a prompt statement?
+alert();
+
+// What do you need in order to capture a user's input in a prompt box? Answer with one word.
+var a; 
+
+var gender = prompt(askGender, bestGuess);
+// In the above statement, what is optional?
+bestGuess
+
+// Create a statement that creates a prompt asking "Your planet?" and assigning the user's response to home.
+var home = prompt("Your planet?", "Mars");
+
+const name = prompt("Please enter your name");
+alert("Hello" + name + " Good morning!");
+
+// Code a prompt with the message "Enter first name". The user's response is assigned to firstName, which hasn't been declared beforehand.
+var firstName = prompt("Enter first name" , "Bob Bobinson");
+// default value is Bob Bobinson
+
+// Code a prompt with the message "Country?" and the default answer "China". The user's response is assigned to country, which hasn't been declared beforehand.
+var country = prompt("Country?", "China");
+
+// Code a prompt that specifies a string as the message. Assign the user's response to a variable that hasn't been declared beforehand.
+var message = prompt("what is your favorite food?");
+
+// Code a prompt that specifies a string as the message. Include a default input. Assign the user's response to a variable that hasn't been declared beforehand.
+var messageTwo = prompt("Enter your name", "Justin");
+
+// Code a prompt that specifies a variable as the message and assigns the user's response to a variable that hasn't been declared beforehand.
+var total = alert(cost);
+
+// Code a prompt that specifies a variable as the message, a second variable as the default response, and assigns the user's response to a third variable that hasn't been declared beforehand.
+var homeCountry = prompt(country, USA);
+
+// Assign strings to two variables. Code a prompt specifying the first variable as the message and the second variable as the default response. Assign the user's response to a third variable. None of the variables have been declared beforehand.
+var responseOne = "No";
+var responseTwo = "Yes";
+var question = prompt(responseOne, responseTwo);
+
+var sex = prompt(askGender, bestGuess);
+// Re-code the statement above, omitting what's optional.
+var sex = prompt(askGender);
+
+// Code a prompt that specifies a string as the message. Assign the user's response to a variable that hasn't been declared beforehand
+
+// Code a prompt that specifies a string as the message. Include a default input. Assign the user's response to a variable that hasn't been declared beforehand.
+var userName = ("Enter Name", "Bob Bobinson");
+
+// Code a prompt that specifies a variable as the message and assigns the user's response to a variable that hasn't been declared beforehand.
+var question = prompt(response);
+
+// Code a prompt that specifies a variable as the message, a second variable as the default response, and assigns the user's response to a third variable that hasn't been declared beforehand.
+var cost = prompt(response, defAwnser);
+
+// Assign strings to two variables. Code a prompt specifying the first variable as the message and the second variable as the default response. Assign the user's response to a third variable. None of the variables have been declared beforehand.
+const yes = "yes";
+const no = "no";
+let question = prompt(yes, no);
+
+// Display a prompt, including both a message and a default response.
+// Display the user's response in an alert.
+// Click the Run It button to run your code live.
+var message = prompt("response", defAwnser);
+alert(message);
+
+// Code an alert that displays...
+// ...the concatenation of a response to a prompt plus a string.
+// Click the Run It button to run your code live.
+alert(prompt("what is your favorite color?") + "is your favorite color");
+
+alert(prompt("What is your name ")+ " ,Good Morning")
